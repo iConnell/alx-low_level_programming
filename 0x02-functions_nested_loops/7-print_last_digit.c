c2037e74401905582159ae134480bfade796eb65
McConnell AlX <ik.ugwuanyi@gmail.com>
2022-09-13 08:17:37 -0700

    moved declaration to the top of the block

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,13 +9,14 @@
 int print_last_digit(int n)
 {
 
+int last_digit;
 if (n < 0)
 {
 n = n * -1;
 }
 
-int last_digit = n % 10;
-_putchar('0' + last);
+last_digit = n % 10;
+_putchar('0' + last_digit);
 
 return (last_digit);
 
