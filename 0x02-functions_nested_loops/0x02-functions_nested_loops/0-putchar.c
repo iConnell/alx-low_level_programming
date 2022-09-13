1268eed83bd07e1cee911b5eface5d702d10fb83
McConnell AlX <ik.ugwuanyi@gmail.com>
2022-09-13 02:05:44 -0700

    0-putchar.c + updated readme

---
+++ 0x02-functions_nested_loops/0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/0x02-functions_nested_loops/README.md
@@ -0,0 +1,8 @@
+0x02-functions_nested_loops
+
+
+### [0-putchar.c](./0-putchar.c)
+```
+prints the string _putchar followed by a new line
+```
+
