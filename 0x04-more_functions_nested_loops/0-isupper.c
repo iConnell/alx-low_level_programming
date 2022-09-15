55b392f471848a7168fd708c4bf8246ca3b0eeed
McConnell AlX <ik.ugwuanyi@gmail.com>
2022-09-15 01:05:44 -0700

    0-isupper.c + README + main.h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isupper - checks is letter is upper case
+ * @c : letter to be checked
+ * Return: int
+ */
+
+int _isupper(int c)
+{
+
+if (c >= 65 && c <= 90)
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,9 @@
+0x04-more_functions_nested_loops
+
+
+### [0-isupper.c](./0-isupper.c)
+```
+checks if a letter is uppercase
+```
+
+
