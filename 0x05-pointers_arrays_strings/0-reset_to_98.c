545e86d5c66d08e802dbc9d4e57802598f901b50
McConnell AlX <ik.ugwuanyi@gmail.com>
2022-09-19 06:51:30 -0700

    0-reset_to_98.c + updated readme

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets value a pointer is pointing to, to 98
+ * @n: pointer
+ * Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+
+*n = 98;
+
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,9 @@
 # 0x05-pointers_arrays_strings
+
+
+### [0-reset_to_98.c](./0-reset_to_98.c)
+```
+resets the value of the variable a pointer is pointing to, to 98
+```
+
+
