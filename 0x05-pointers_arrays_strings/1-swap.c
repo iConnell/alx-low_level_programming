d6472d7ea390aaf380367efeefffb619989788e7
McConnell AlX <ik.ugwuanyi@gmail.com>
2022-09-19 07:09:05 -0700

    1-swap.c + updated readme + main.h

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: first pointer
+ * @b: second pointer
+ * Return: nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -7,3 +7,9 @@ resets the value of the variable a pointer is pointing to, to 98
 ```
 
 
+### [1-swap.c](./1-swap.c)
+```
+swaps the values of two variables
+```
+
+
