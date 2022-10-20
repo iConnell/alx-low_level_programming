c4796de5abd85383d49f181970a46d86643b1ef4
McConnell AlX <ik.ugwuanyi@gmail.com>
2022-10-20 14:27:17 -0700

    moved increament outside if condition

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,17 +6,18 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t count = 0;
-while (h != NULL)
-{
-if (h->str == NULL)
-printf("[%d] (nil)\n", 0);
-else
-{
-printf("[%d] %s\n", h->len, h->str);
-h = h->next;
-count++;
-}
-}
-return (count);
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] (nil)\n", 0);
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
