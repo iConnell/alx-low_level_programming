3acdcacb8948313bc1b6fe95ff6227e4ec05ae28
McConnell AlX <ik.ugwuanyi@gmail.com>
2022-10-20 14:01:11 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "list.h"
+/**
+ * print_list - prints all the elements  a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+size_t print_list(const list_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[%d] (nil)\n", 0);
+else
+{
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+count++;
+}
+}
+return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,7 @@
+0x12-singly_linked_lists
+
+### [0-print_list.c](./0-print_list.c)
+```
+prints all the elements of a linked list
+```
+
